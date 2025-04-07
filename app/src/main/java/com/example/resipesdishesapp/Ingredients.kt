bb0665601b8c140bf8ddea2db0b0ae2271302d40
Evgeniy <Evgrazhev@yandex.ru>
2025-04-07 10:49:34 +0300

    RA-06-dataClass

---
+++ app/src/main/java/com/example/resipesdishesapp/Ingredients.kt
@@ -0,0 +1,8 @@
+package com.example.resipesdishesapp
+
+data class Ingredients(
+    val quantity: String,
+    val unitOfMeasure: String,
+    val description: String
+) {
+}
---
+++ app/src/main/java/com/example/resipesdishesapp/Recipe.kt
@@ -0,0 +1,8 @@
+package com.example.resipesdishesapp
+
+data class Recipe(
+    val id: Int,
+    val title: String,
+    val ingredients: Ingredients
+) {
+}
