249a17943d06d9f31aea971ce9f00b17062be9ff
Евгений <evgrazhev@yandex.ru>
2025-07-29 23:20:26 +0300

    RA-37-coroutine-refactor

--- app/src/main/java/com/example/resipesdishesapp/data/RecipesRepository.kt
+++ app/src/main/java/com/example/resipesdishesapp/data/RecipesRepository.kt
@@ -26,16 +26,8 @@ class RecipesRepository {
     suspend fun getCategories(): NetworkResult<List<Category>> {
         return withContext(Dispatchers.IO) {
             try {
-                val categoriesResponse = service.getCategories().execute()
-                if (categoriesResponse.isSuccessful) {
-                    categoriesResponse.body()?.let {
-                        NetworkResult.Success(it)
-                    } ?: run {
-                        NetworkResult.Error(R.string.errorData)
-                    }
-                } else {
-                    NetworkResult.Error(R.string.errorConnect)
-                }
+                val categoriesResponse = service.getCategories()
+                NetworkResult.Success(categoriesResponse)
             } catch (e: Exception) {
                 NetworkResult.Error(R.string.errorConnect)
             }
@@ -45,16 +37,8 @@ class RecipesRepository {
     suspend fun getRecipesCategoryId(categoryId: Int): NetworkResult<List<Recipe>> {
         return withContext(Dispatchers.IO) {
             try {
-                val recipesResponse = service.getRecipesByCategoryId(categoryId).execute()
-                if (recipesResponse.isSuccessful) {
-                    recipesResponse.body()?.let {
-                        NetworkResult.Success(it)
-                    } ?: run {
-                        NetworkResult.Error(R.string.errorData)
-                    }
-                } else {
-                    NetworkResult.Error(R.string.errorConnect)
-                }
+                val recipesResponse = service.getRecipesByCategoryId(categoryId)
+                NetworkResult.Success(recipesResponse)
             } catch (e: Exception) {
                 NetworkResult.Error(R.string.errorConnect)
             }
@@ -64,16 +48,8 @@ class RecipesRepository {
     suspend fun getRecipeById(recipeId: Int): NetworkResult<Recipe> {
         return withContext(Dispatchers.IO) {
             try {
-                val recipeResponse = service.getRecipeById(recipeId).execute()
-                if (recipeResponse.isSuccessful) {
-                    recipeResponse.body()?.let {
-                        NetworkResult.Success(it)
-                    } ?: run {
-                        NetworkResult.Error(R.string.errorData)
-                    }
-                } else {
-                    NetworkResult.Error(R.string.errorConnect)
-                }
+                val recipeResponse = service.getRecipeById(recipeId)
+                NetworkResult.Success(recipeResponse)
             } catch (e: Exception) {
                 NetworkResult.Error(R.string.errorConnect)
             }
@@ -84,16 +60,8 @@ class RecipesRepository {
         return withContext(Dispatchers.IO) {
             try {
                 val idsString = recipesId.joinToString(",")
-                val recipesResponse = service.getListRecipeId(idsString).execute()
-                if (recipesResponse.isSuccessful) {
-                    recipesResponse.body()?.let {
-                        NetworkResult.Success(it)
-                    } ?: run {
-                        NetworkResult.Error(R.string.errorData)
-                    }
-                } else {
-                    NetworkResult.Error(R.string.errorConnect)
-                }
+                val recipesResponse = service.getListRecipeId(idsString)
+                NetworkResult.Success(recipesResponse)
             } catch (e: Exception) {
                 NetworkResult.Error(R.string.errorConnect)
             }
